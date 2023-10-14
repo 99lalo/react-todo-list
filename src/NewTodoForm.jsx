2f626e1f48f1fd39bed8762cc26a18bdee888bb3
Franky Lopez <74028591+99lalo@users.noreply.github.com>
2023-10-14 17:26:44 -0400

    Finished React Basic Course

---
+++ src/NewTodoForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+export function NewTodoForm({ onSubmit }) {
+
+NewTodoForm.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+};
+
+  const [newItem, setNewItem] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (newItem === "") return;
+
+    onSubmit(newItem);
+
+    setNewItem("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input
+          type="text"
+          id="item"
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+        />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+  );
+}
---
+++ src/TodoList.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import { TodoItem } from "./TodoItem";
+
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
+  return (
+    <ul className="list">
+      {todos.length === 0 && "No Todos"}
+      {todos.map((todo) => (
+        <TodoItem
+          {...todo}
+          key={todo.id}
+          toggleTodo={toggleTodo}
+          deleteTodo={deleteTodo}
+        />
+      ))}
+    </ul>
+  );
+}
+
+TodoList.propTypes = {
+  todos: PropTypes.array.isRequired,
+  toggleTodo: PropTypes.func.isRequired,
+  deleteTodo: PropTypes.func.isRequired,
+};
+
